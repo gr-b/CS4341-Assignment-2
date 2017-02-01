9761dfdebf63a42d95d2421295af18daf3bfab47
ddeisadze <tbilisidavid@hotmail.com>
2017-02-01 11:59:29 -0500

    FInalized first-choice hill climbing

--- optimize.py
+++ optimize.py
@@ -2,6 +2,7 @@
 # Griffin Bishop, David Deisde, Gianluca Tarquinio, Ian Vossoughi
 
 import sys, random
+from random import randint
 
 def getFromFile(filename):
     file = open(filename,"r")
@@ -86,14 +87,92 @@ def scoreBins(bins):
    print("Second bin: " + str(scoreBin2(bins[1])))
    print("Third bin: " + str(scoreBin3(bins[2])))
 
-def hillClimbing(numbers):
+def getAllBinScores(bins):
+    """
+    Returns a tuple length of 3 with the scores of respective bins
+    :param bins: The bins that the numbers are dropped in
+    :return: Tuple() of scores for the 3 bins
+    """
+    return (scoreBin1(bins[0]), scoreBin2(bins[1]), scoreBin3(bins[2]))
+
+def moveOperator():
+    """
+    Determines all possible moves for a given solution
+    :return:
+    """
+
+def hillClimbing(bins, max_tries=100):
     """
     Performs hill climbing to find best scored bins for the given numbers.
 
     :param numbers: Input numbers passed in
     :return: None
     """
-    pass
+    # Bin Swapping logic: get random bin, and random value from that bin. Same for other bin, if their swap beats the best score, then make the swap
+
+    best_score_individual = getAllBinScores(bins)
+    best_score = sum(best_score_individual)
+
+    num_bins = len(bins)
+    tries = 0
+
+    while tries < max_tries:
+        # do we want to do states that we have already seen?
+        # already_seen_move = []
+        # new_bins = bins.pop(cur_i)
+
+        # get the two random bin indices
+        starting_bin_ind = getRandomBin(bins)
+        other_bin_ind = getRandomBin(bins)
+
+        # get the two random bin value indices
+        starting_bin_val_ind = getRandomNumInBin(bins)
+        other_bin_val_ind = getRandomNumInBin(bins)
+
+        # check to see if the same value is not selected in the same bin
+        while other_bin_ind == starting_bin_ind and starting_bin_val_ind == other_bin_val_ind:
+            other_bin_val_ind = getRandomNumInBin(bins)
+
+        # make swap here
+        bins[starting_bin_ind][starting_bin_val_ind] = bins[other_bin_ind][other_bin_val_ind]
+        tries += 1
+
+        # get the current score after the swap
+        score = sum(getAllBinScores(bins))
+
+        # check the score against the best score, if the swap was successfull, finalize the change, otherwise swap back
+        if score > best_score:
+            best_score = score
+            continue
+        else:
+            # swap them back
+            bins[starting_bin_ind][starting_bin_val_ind] = bins[other_bin_ind][other_bin_val_ind]
+
+    return bins
+
+
+def getRandomBin(bins):
+    """
+    Gets a random bin
+    :param bins:  the bins
+    :return: returns a random bin index
+    """
+    bin_length = len(bins)
+    index = randint(0, bin_length-1)
+
+    return index
+
+def getRandomNumInBin(bins):
+    """
+    Gets random number from the passed in bin
+    :param bin: the bin chosen to get val from
+    :return: random value within the bin
+    """
+    bin_size = len(bins)
+    index = randint(0,bin_size-1)
+    # val = bin[rand_int]
+    return index
+
 
 def main():
     arguments = sys.argv
@@ -111,6 +190,9 @@ def main():
     bins = putInBins(nums)
     printBins(bins)
     scoreBins(bins)
+
+    # hill climbing tests
+    hillClimbing(bins)
 
 if __name__ == '__main__':
     main()
